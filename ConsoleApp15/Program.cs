00954a69e28504111fd39980fc02053d9cc2eaf5
AlexxNikolaev <143645546+AlexxNikolaev@users.noreply.github.com>
2023-12-24 23:58:01 +0300

    Добавьте файлы проекта.

---
+++ ConsoleApp15/Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+public class PhoneBookEntry
+{
+    public string FirstName { get; set; }
+    public string LastName { get; set; }
+    public string PhoneNumber { get; set; }
+}
+
+public class PhoneBook
+{
+    private List<PhoneBookEntry> entries = new List<PhoneBookEntry>();
+
+    public void AddEntry(string firstName, string lastName, string phoneNumber)
+    {
+        entries.Add(new PhoneBookEntry
+        {
+            FirstName = firstName,
+            LastName = lastName,
+            PhoneNumber = phoneNumber
+        });
+    }
+
+    public List<PhoneBookEntry> Search(string query)
+    {
+        return entries.Where(entry =>
+            entry.FirstName.ToLower().Contains(query.ToLower()) ||
+            entry.LastName.ToLower().Contains(query.ToLower()) ||
+            entry.PhoneNumber.Contains(query)).ToList();
+    }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        PhoneBook phoneBook = new PhoneBook();
+
+       
+        phoneBook.AddEntry("John", "Doe", "123-456-7890");
+        phoneBook.AddEntry("Jane", "Smith", "234-567-8901");
+        phoneBook.AddEntry("Alice", "Johnson", "345-678-9012");
+
+       
+        Console.Write("Enter first/last name or phone number to search: ");
+        string query = Console.ReadLine();
+
+        List<PhoneBookEntry> results = phoneBook.Search(query);
+
+        if (results.Count == 0)
+        {
+            Console.WriteLine("No matching entries found.");
+        }
+        else
+        {
+            Console.WriteLine("Matching entries:");
+            foreach (var entry in results)
+            {
+                Console.WriteLine($"First Name: {entry.FirstName}, Last Name: {entry.LastName}, Phone Number: {entry.PhoneNumber}");
+            }
+        }
+    }
+}
+
